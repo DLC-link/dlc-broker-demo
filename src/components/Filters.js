10f805a222b6415642f3dcce9b72abf955ba6417
Polybius93 <99192647+Polybius93@users.noreply.github.com>
2023-04-05 15:38:57 +0200

    Changed colors, added filter button (#5)

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -5,74 +5,83 @@ import {
     receivedFilterChanged,
 } from '../store/vaultsSlice';
 
-import { Text, HStack, Flex, VStack, Switch } from '@chakra-ui/react';
+import {
+    Text,
+    HStack,
+    Flex,
+    VStack,
+    Switch,
+    Collapse,
+    Button,
+} from '@chakra-ui/react';
 import { selectFilters } from '../store/vaultsSlice';
 
 export default function Filters() {
     const filters = useSelector(selectFilters);
     const dispatch = useDispatch();
+    const [showFilters, setShowFilters] = useState(false);
+
     return (
         <>
-            <VStack
-                padding="10px"
-                height="auto"
-                width="350px"
-                border="1px"
-                borderRadius="lg"
-                borderColor="white"
-                shadow="dark-lg"
-                justifyContent="center"
-                align="left"
+            <Button
+                variant={'outline'}
+                width={'350px'}
+                onClick={() => setShowFilters(!showFilters)}
             >
-                <HStack justify="space-between">
-                    <Text
-                        fontSize="small"
-                        display="inline"
-                        fontWeight="extrabold"
-                        color="white"
-                    >
-                        Show vaults{' '}
-                        <Text
-                            fontSize="small"
-                            fontWeight="extrabold"
-                            as="span"
-                            color="primary1"
-                        >
-                            created by me
-                        </Text>
-                    </Text>
-
-                    <Switch
-                        colorScheme="teal"
-                        size="sm"
-                        isChecked={filters.showMinted}
-                        onChange={(e) =>
-                            dispatch(mintedFilterChanged(e.target.checked))
-                        }
-                    />
-                </HStack>
-                <HStack justify="space-between">
-                    <Text fontSize="small" fontWeight="extrabold" color="white">
-                        Show vaults{' '}
-                        <Text
-                            fontSize="small"
-                            fontWeight="extrabold"
-                            as="span"
-                            color="yellow"
-                        >
-                            NOT created by me
-                        </Text>
-                    </Text>
-                    <Switch
-                        colorScheme="teal"
-                        size="sm"
-                        isChecked={filters.showReceived}
-                        onChange={(e) =>
-                            dispatch(receivedFilterChanged(e.target.checked))
-                        }
-                    />
-                </HStack>
-            </VStack>
+                SHOW FILTERS
+            </Button>
+            <Collapse in={showFilters}>
+                <VStack
+                    padding="15px"
+                    height="auto"
+                    width="350px"
+                    border="1px"
+                    borderRadius="lg"
+                    borderColor="white"
+                    shadow="dark-lg"
+                    justifyContent="center"
+                    alignItems="justify"
+                >
+                    <HStack justify="space-between">
+                            <Text
+                                fontSize="small"
+                                fontWeight="extrabold"
+                                as="span"
+                                color="accent"
+                            >
+                                CREATED BY ME
+                            </Text>
+                        <Switch
+                            colorScheme="teal"
+                            size="sm"
+                            isChecked={filters.showMinted}
+                            onChange={(e) =>
+                                dispatch(mintedFilterChanged(e.target.checked))
+                            }
+                        />
+                    </HStack>
+                    <HStack justify="space-between">
+                            <Text
+                                fontSize="small"
+                                fontWeight="extrabold"
+                                as="span"
+                                color='accent'
+                            >
+                                NOT CREATED BY ME
+                            </Text>
+                        <Switch
+                            colorScheme="teal"
+                            size="sm"
+                            isChecked={filters.showReceived}
+                            onChange={(e) =>
+                                dispatch(
+                                    receivedFilterChanged(e.target.checked)
+                                )
+                            }
+                        />
+                    </HStack>
+                </VStack>
+            </Collapse>
         </>
     );
 }
